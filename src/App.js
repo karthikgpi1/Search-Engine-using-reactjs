a3bfe6faa5f9dca1c4f16ceacdf55f7e64885747
karthikgpi1 <karthikgopi024@gmail.com>
2022-09-15 20:10:20 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { Routes } from './components/Routes';
+import { Path } from './components/Path';
 
 
 
@@ -15,7 +15,7 @@ const App = () => {
     <div className={darkTheme ? 'dark' : '' }>
        <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
           <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-          <Routes />
+          <Path />
           <Footer />
        </div>
     </div>
